53e46044d1ce5ae1735e868069dbe06f3ec11727
Tykky <40919314+Tykky@users.noreply.github.com>
2020-11-23 22:55:22 +0200

    kohta 4

--- Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import com.google.common.base.Verify;
+
 public class KauppaTest {
 
     @Test
@@ -150,5 +152,46 @@ public class KauppaTest {
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(),eq(5));   
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
+
+
+    @Test
+    public void testi5() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.saldo(2)).thenReturn(0);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "mehu", 4));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(),eq(5));   
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+
+        when(viite.uusi()).thenReturn(43);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("asd", "44");
+
+        verify(pankki).tilisiirto(eq("asd"), eq(43), eq("44"), anyString(),eq(5));   
+
+    }
+    
     
 }
