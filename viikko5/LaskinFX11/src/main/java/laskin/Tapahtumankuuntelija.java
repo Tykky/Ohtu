72ea4afc0378105dd209479bdf4ea15cc2524644
Tykky <40919314+Tykky@users.noreply.github.com>
2020-11-30 19:50:07 +0200

    tehtävä 5

--- viikko5/LaskinFX11/src/main/java/laskin/Tapahtumankuuntelija.java
+++ viikko5/LaskinFX11/src/main/java/laskin/Tapahtumankuuntelija.java
@@ -5,13 +5,32 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+
+public abstract class Komento {
+    protected IO io;
+    Io revert;
+
+    public Komento(IO io, Io revert) {
+        this.io = io;
+        this.revert = revert;
+    }
+
+    public void suorita() {
+        this.io();
+    }
+
+    public void peruuta() {
+        this.revert();
+    }
+
+}
+
 public class Tapahtumankuuntelija implements EventHandler {
     private Button undo;
     private Sovelluslogiikka sovellus;
     
     private Map<Button, Komento> komennot;
     private Komento edellinen = null;
- 
 
     public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
         this.undo = undo;
@@ -21,7 +40,6 @@ public class Tapahtumankuuntelija implements EventHandler {
         this.komennot.put(nollaa, new Nollaa(tuloskentta, syotekentta,  nollaa, undo, sovellus) );
         this.komennot.put(plus, new Summa(tuloskentta, syotekentta,  nollaa, undo, sovellus) );
     }
-    
     @Override
     public void handle(Event event) {
         if ( event.getTarget() != undo ) {
